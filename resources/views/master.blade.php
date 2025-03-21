68c2ba3de6b86264462cff2758f1f833c90bed5f
MohamadHosinMohajer <105666603+mohamadhosinmoh5@users.noreply.github.com>
2025-03-21 06:00:22 -0700

    1

---
+++ resources/views/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>فروشگاه</title>
+    @livewireStyles
+</head>
+<body>
+    @yield('content')
+    @livewireScripts
+</body>
+</html>
